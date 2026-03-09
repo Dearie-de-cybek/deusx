d5c75d1e1f840e2478fd4947e7e6ffb78d976f09
Dearie <eburujacintha@gmail.com>
2026-03-09 09:45:46 +0100

    added services section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "../app/components/layout/Navbar";
 import Hero from "../app/components/sections/Hero";
+import Services from "./components/sections/Services";
 
 export default function Home() {
   return (
     <main className="relative bg-brand-bg">
       <Navbar />
       <Hero />
+      <Services />
       {/* Future sections (Work, Services) go here */}
       <div className="h-screen" /> {/* Spacer for scroll testing */}
     </main>
